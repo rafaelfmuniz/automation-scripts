63f3c1c2b4fd665d4ccb2da29420ba2314ea61b0
rafaelfmuniz <121843852+rafaelfmuniz@users.noreply.github.com>
2025-02-28 18:58:34 -0700

    Update nextcloud-aio_auto-update.sh

--- nextcloud-aio_auto-update.sh
+++ nextcloud-aio_auto-update.sh
@@ -3,62 +3,10 @@
 SCRIPT_NAME="nextcloud-aio_auto-update.sh"
 SCRIPT_PATH="/root/$SCRIPT_NAME"
 LOG_FILE="/var/log/nextcloud_aio_auto-update.log"
-PYTHON_SCRIPT="/tmp/get_schedule_time.py"
+SCHEDULE_TIME="04:00"
+CRON_JOB="0 4 * * * /root/$SCRIPT_NAME >> $LOG_FILE 2>&1"
 
 echo "$(date) ---- Configurando automação Nextcloud AIO ----" | tee -a "$LOG_FILE"
-
-# Criar script Python
-cat << 'EOF' > "$PYTHON_SCRIPT"
-#!/usr/bin/env python3
-
-while True:
-    print("Escolha o horário de agendamento:")
-    print("1. 4am")
-    print("2. 5am")
-    print("3. 6am")
-
-    try:
-        choice = input("Digite o número da opção desejada: ")
-    except EOFError:
-        print("\\nEntrada inválida. Saindo.")
-        exit(1)
-
-    if choice in ("1", "2", "3"):
-        break
-    else:
-        print("Opção inválida.")
-
-if choice == "1":
-    schedule_time = "04:00"
-elif choice == "2":
-    schedule_time = "05:00"
-else:
-    schedule_time = "06:00"
-
-try:
-    confirm = input(f"Confirma o horário de agendamento {schedule_time}? (s/n): ")
-except EOFError:
-    print("\\nEntrada inválida. Saindo.")
-    exit(1)
-
-if confirm == "s":
-    print(schedule_time)
-else:
-    print("")
-EOF
-
-chmod +x "$PYTHON_SCRIPT"
-
-SCHEDULE_TIME="$("$PYTHON_SCRIPT")"
-
-if [[ -z "$SCHEDULE_TIME" ]]; then
-    echo "$(date) [ERRO] Agendamento cancelado." | tee -a "$LOG_FILE"
-    exit 1
-fi
-
-SCHEDULE_HOUR=$(echo "$SCHEDULE_TIME" | cut -d ':' -f 1)
-SCHEDULE_MINUTE=$(echo "$SCHEDULE_TIME" | cut -d ':' -f 2)
-CRON_JOB="$SCHEDULE_MINUTE $SCHEDULE_HOUR * * * /root/$SCRIPT_NAME >> $LOG_FILE 2>&1"
 
 # Criar script local
 cat << "EOF" > "$SCRIPT_PATH"
